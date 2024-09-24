40c0a9dea054b6d6a59443fc141d07e2f8e38556
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2024-09-24 17:44:47 -0300

    :wrench: chore: add configs

--- src/utils/move_to_file/test_move_to_file.py
+++ src/utils/move_to_file/test_move_to_file.py
@@ -1,32 +1,31 @@
-import unittest2
-from unittest2 import mock
+import pytest
+import os
+from src.utils.logging.log_manager.log_manager import write_to_log  # Adjust based on your actual path
+from src.utils.move_to_file.move_to_file import move_to_file  # Adjust based on your actual path
 
-from move_to_file import move_to_file
+def test_move_to_file(mocker):
+    # Mock os.rename
+    mock_rename = mocker.patch('os.rename')
+    # Mock write_to_log
+    mock_write_to_log = mocker.patch('src.utils.logging.log_manager.log_manager.write_to_log')
+    
+    source = "test_source.txt"
+    destination = "test_destination.txt"
+    
+    # Call the function
+    move_to_file(source, destination)
+    
+    # Assertions to verify the mocks were called correctly
+    mock_rename.assert_called_once_with(source, destination)
+    mock_write_to_log.assert_called_once_with(f"File moved from {source} to {destination}")
 
-class TestMoveToFile(unittest2.TestCase):
-    @mock.patch('os.rename')
-    def test_move_to_file_success(self, mock_rename):
-        # Define source and destination
-        source = "source.txt"
-        destination = "destination.txt"
-        
-        # Call the function
+def test_move_to_file_file_not_found(mocker):
+    # Mock os.rename to raise a FileNotFoundError
+    mock_rename = mocker.patch('os.rename', side_effect=FileNotFoundError)
+    
+    source = "non_existent_file.txt"
+    destination = "any_destination.txt"
+    
+    # Test if the FileNotFoundError is raised
+    with pytest.raises(FileNotFoundError):
         move_to_file(source, destination)
-
-        # Check if os.rename was called with the correct arguments
-        mock_rename.assert_called_once_with(source, destination)
-
-    @mock.patch('os.rename', side_effect=OSError("File not found"))
-    def test_move_to_file_failure(self, mock_rename):
-        
-
-        source = "nonexistent.txt"
-        destination = "destination.txt"
-        
-        with self.assertLogs(level='INFO') as log:
-            move_to_file(source, destination)
-        
-        self.assertIn("An error occurred: File not found", log.output[0])
-
-if __name__ == '__main__':
-    unittest2.main()
