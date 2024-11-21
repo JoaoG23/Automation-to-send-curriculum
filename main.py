cd0540d0569b99da7e5debb33ae23222456c5811
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2024-11-21 17:30:02 -0300

    :bug: fix: Fix error not found image email

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import csv
 from tkinter import *
-from tkinter import ttk
+import traceback
 
 import pyautogui
+from pyautogui import ImageNotFoundException
 from time import sleep
 from datetime import datetime
 
@@ -11,12 +12,7 @@ from datetime import datetime
 from src.utils.logging.log_manager.log_manager import write_to_log
 from src.utils.send_email_for_job.send_email_for_job import send_email_for_job
 from src.utils.move_to_file.move_to_file import move_to_file
-# root = Tk()
-# frm = ttk.Frame(root, padding=30)
-# frm.grid()
-# ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-# ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-# root.mainloop()
+
 if __name__ == '__main__':
     try:
         
@@ -25,15 +21,19 @@ if __name__ == '__main__':
         # pyautogui.PAUSE = 1
         # pyautogui.position()
         icon_navbar_path = os.path.join(os.getcwd(), 'assets', 'icon_navbar.png')
-        icon_navbar_position = pyautogui.locateCenterOnScreen(icon_navbar_path)
+        icon_navbar_position = pyautogui.locateCenterOnScreen(icon_navbar_path , grayscale=True, confidence=.7)
         pyautogui.click(icon_navbar_position, duration=0.3)
         sleep(10)
-
         
-        # image_email_path = os.path.join(os.getcwd(), 'assets', 'email.png')
-        # email_select_position = pyautogui.locateCenterOnScreen(image_email_path)
+        icon_maximize_window_path = os.path.join(os.getcwd(), 'assets', 'maximize_button.png')
+        icon_maximize_window_path = pyautogui.locateCenterOnScreen(icon_navbar_path , grayscale=True, confidence=.7)
+        pyautogui.click(icon_navbar_position, duration=0.3)
+        sleep(4)
         
-        pyautogui.click(231,322, duration=0.1)
+        image_email_path = os.path.join(os.getcwd(), 'assets', 'email.png')
+        email_select_position = pyautogui.locateCenterOnScreen(image_email_path)
+        
+        # pyautogui.click(231,322, duration=0.1)
         sleep(2)
         
         datetime_now = datetime.now().strftime('%Y%m%d%H%M%S')
@@ -54,6 +54,12 @@ if __name__ == '__main__':
         
         move_to_file(path_file_imports, path_file_export)
         write_to_log(f'Curriculums sent with success!{len(list_file)}')
+    except KeyboardInterrupt as e:
+        print("KeyboardInterrupt : Interrupted by user.")
+        write_to_log(f"KeyboardInterrupt: {traceback.format_exc()}", type='error')
+    except ImageNotFoundException as e:
+        print("ImageNotFoundException : Image not found.")
+        write_to_log(f"ImageNotFoundException: {traceback.format_exc()}", type='error')
     except Exception as e:
-        write_to_log(f"Error sending curriculums: {e}", type='error')
-        raise e
+        print("RuntimeError : An error occurred on app.")
+        write_to_log(f"Error {traceback.format_exc()}", type='error')
